7618556a20e0002ef24572bf49eb8e8457049c22
IvanoPisaniello <ivanop92@gmail.com>
2023-09-04 10:04:28 +0200

    added dropdowns on navbar

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,4 +2,20 @@
 export default {
 
 }
+
 </script>
+
+<template>
+    <div class="footer-container">
+
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.footer-container {
+    border-top: 5px solid #333333;
+    height: 400px;
+    background-color: #BF1E2E;
+}
+</style>
